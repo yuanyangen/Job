70ff6f51a114cf340a6dc44ef942186c70c48103
yuanyangen <yuanyangen@gmail.com>
2016-05-23 11:12:34 +0800

    添加二叉查找树的代码

---
+++ src/tree/RBTree.go
@@ -0,0 +1,64 @@
+package tree
+
+
+//这个文件的代码实现了红黑树的节点添加， 查找的问题
+type TreeNode struct {
+	key   int
+	left  *TreeNode
+	right *TreeNode
+}
+
+var RootNode = &TreeNode{}
+
+//初始化一颗二叉树
+func init() {
+	RootNode = &TreeNode{}
+	RootNode.key = 10
+	addNode(1, nil)
+	addNode(2, nil)
+	addNode(3, nil)
+	addNode(20, nil)
+
+}
+
+//添加一个节点， 如果Node为nil 表示为空节点
+func addNode(val int, Node * TreeNode) {
+	if Node == nil {
+		Node = &RootNode
+	}
+	//val已经存在
+	if Node.key == val {
+		panic("value already exists")
+	} else if Node.key > val {
+		if Node.left != nil {
+			addNode(val, Node.left)
+		} else {
+			Node.left = &TreeNode{}
+			Node.left.key = val
+		}
+
+	} else {
+		if Node.right != nil {
+			addNode(val,Node.right)
+		} else {
+			Node.right = &TreeNode{}
+			Node.right.key = val
+		}
+	}
+}
+
+func Query(key int ) (int, error) {
+	return QueryNode(key, RootNode)
+}
+
+ func QueryNode(key, Node *TreeNode) {
+	 if key  == Node.key {
+		 return Node.key, nil
+	 } else if key < Node.key {
+		 return QueryNode(key, Node.left)
+	 } else {
+		 return QueryNode(key, Node.right)
+	 }
+ }
+
+
